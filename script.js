e31db537655742155e6d178a62e50a165bb0f679
arnarsmara <arnarsma@msn.com>
2026-02-20 10:34:14 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -34,3 +34,39 @@ function logout() {
   localStorage.removeItem("loggedIn");
   window.location.href = "login.html";
 }
+
+function updateFlightStatus(flightTimeStr, elementId) {
+  const flightTime = new Date(flightTimeStr);
+  const now = new Date();
+
+  const diffMinutes = (flightTime - now) / 1000 / 60;
+  const box = document.getElementById(elementId);
+
+  if (!box) return;
+
+  const text = box.querySelector(".text");
+
+  box.className = "flight-status";
+
+  if (diffMinutes <= 0) {
+    text.textContent = "Departed";
+    box.classList.add("departed");
+  } else if (diffMinutes <= 15) {
+    text.textContent = "Final call";
+    box.classList.add("final");
+  } else if (diffMinutes <= 45) {
+    text.textContent = "Boarding";
+    box.classList.add("boarding");
+  } else {
+    text.textContent = "On time";
+  }
+}
+
+// Uppfæra strax + á 1 mín fresti
+function updateAllFlights() {
+  updateFlightStatus("2026-03-12T18:50:00", "outbound-status");
+  updateFlightStatus("2026-03-16T08:35:00", "return-status");
+}
+
+updateAllFlights();
+setInterval(updateAllFlights, 60000);
